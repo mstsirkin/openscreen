2b18b58058df0b1a9b6efaf16e85401fe7574ff7
Michael S. Tsirkin <mst@redhat.com>
2026-04-03 13:15:36 -0400

    Use current cast play state for picked files

--- android-app/app/src/main/java/org/openscreen/controlcast/MainActivity.kt
+++ android-app/app/src/main/java/org/openscreen/controlcast/MainActivity.kt
@@ -1552,7 +1552,7 @@ private fun ControlCastApp(testTarget: String? = null, testFile: String? = null,
         if (uri != null) {
             pendingExternalPlayUri = null
             val shouldStartPlaying = if (connectionState == Connection.State.CONNECTED) {
-                true
+                connection.isCastPlaying()
             } else {
                 localMirrorEnabled
             }
