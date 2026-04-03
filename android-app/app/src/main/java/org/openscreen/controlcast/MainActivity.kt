1692a3cb474779573d5893636f543eae9d905746
Michael S. Tsirkin <mst@redhat.com>
2026-04-03 13:14:38 -0400

    Start picked files playing while connected

--- android-app/app/src/main/java/org/openscreen/controlcast/MainActivity.kt
+++ android-app/app/src/main/java/org/openscreen/controlcast/MainActivity.kt
@@ -1552,7 +1552,7 @@ private fun ControlCastApp(testTarget: String? = null, testFile: String? = null,
         if (uri != null) {
             pendingExternalPlayUri = null
             val shouldStartPlaying = if (connectionState == Connection.State.CONNECTED) {
-                connection.isCastPlaying() || isPlaying
+                true
             } else {
                 localMirrorEnabled
             }
